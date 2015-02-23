65bde63205f98787554e5b24c76156452234ee9b
A. Emre Ünal <aemreunal@gmail.com>
2015-02-23 18:09:32 +0200

    Test DB configuration

---
+++ src/main/java/config/DatabaseSettings.java
@@ -0,0 +1,46 @@
+package config;
+
+/*
+ ***************************
+ * Copyright (c) 2015      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * emre@aemreunal.com      *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+import java.io.File;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class DatabaseSettings {
+    private static final SessionFactory  ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure(new File("hibernate.cfg.xml"));
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+}
---
+++ src/main/java/domain/Sale.java
@@ -0,0 +1,35 @@
+package domain;
+
+/*
+ ***************************
+ * Copyright (c) 2015      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * emre@aemreunal.com      *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+
+public class Sale {
+//    @Id
+//    @Column(name = "sale_id")
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @OrderColumn
+//    @Access(AccessType.PROPERTY)
+    private int saleId;
+
+    public int getSaleId() {
+        return saleId;
+    }
+
+    public void setSaleId(int saleId) {
+        this.saleId = saleId;
+    }
+}
