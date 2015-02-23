2cecda9f6ec26a94b920bebb0774f57c70d4346f
A. Emre Ünal <aemreunal@gmail.com>
2015-02-23 15:40:10 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+/*
+ ***************************
+ * Copyright (c) 2015      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * emre@aemreunal.com      *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
