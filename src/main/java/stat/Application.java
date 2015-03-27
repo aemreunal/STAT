daeff49f40ae7746db4aa725fc3880512079c1f9
A. Emre Ünal <aemreunal@gmail.com>
2015-03-27 23:57:48 +0200

    Set Look & Feel to system default

--- src/main/java/stat/Application.java
+++ src/main/java/stat/Application.java
@@ -16,12 +16,19 @@ package stat;
  ***************************
  */
 
+import javax.swing.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(final String[] args) {
+        // Set System L&F
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            System.err.println("Unable to set Look & Feel to system default!");
+        }
         SpringApplication app = new SpringApplication(Application.class);
         app.setWebEnvironment(false); // To not instantiate a web environment
         app.setHeadless(false); // To instantiate AWT
