b484d46ff648e5ab2485a5c2b0a3adbcf412d341
bbsarikaya <burcu.sarikaya@ozu.edu.tr>
2015-03-29 19:19:50 +0300

    implement ProductSalesTab class.

--- src/main/java/stat/ui/product/detail/view/tabs/ProductSalesTab.java
+++ src/main/java/stat/ui/product/detail/view/tabs/ProductSalesTab.java
@@ -1,31 +1,47 @@
 package stat.ui.product.detail.view.tabs;
 
-/*
- ***************************
- * Copyright (c) 2015      *
- *                         *
- * This code belongs to:   *
- *                         *
- * @author Ahmet Emre Ünal *
- * S001974                 *
- *                         *
- * aemreunal@gmail.com     *
- * emre.unal@ozu.edu.tr    *
- *                         *
- * aemreunal.com           *
- ***************************
+/**
+ * Author Burcu Basak SARIKAYA
+ * S000855
+ * burcu.sarikaya@ozu.edu.tr
  */
 
+import stat.domain.Sale;
 import stat.ui.Page;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import stat.ui.sale.main.view.helper.SaleTableModel;
+
+import javax.swing.*;
+import java.util.LinkedHashSet;
 
 // Required to not run this class in a test environment
 @ConditionalOnProperty(value = "java.awt.headless", havingValue = "false")
 public class ProductSalesTab extends Page {
 
-    @Override
-    protected void initPage() {
+    private JTable saleTable;
+    private SaleTableModel tableModel;
 
+    public ProductSalesTab() {
+        initPage();
+        initSaleTable();
+    }
+
+    @Override
+    protected void initPage() { setLayout(null); }
+
+    private void initSaleTable() {
+        tableModel = new SaleTableModel();
+        saleTable = new JTable(tableModel);
+        saleTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrollPane = new JScrollPane(saleTable);
+        scrollPane.setBounds(30, 30, 325, 200);
+        add(scrollPane);
+    }
+
+    public void fillSaleTable(LinkedHashSet<Sale> sales) {
+        for(Sale sale: sales) {
+            tableModel.addRow(new Object[]{sale.getCustomerName(), sale.getDate(), sale.getTotalPrice()});
+        }
     }
 }
