5c1e9faa6e5c9e0acb39948c79146f3b4894068e
A. Emre Ünal <aemreunal@gmail.com>
2015-04-05 16:31:43 +0300

    Add fiscal interval summary calculation

--- src/main/java/stat/ui/stats/main/StatController.java
+++ src/main/java/stat/ui/stats/main/StatController.java
@@ -14,11 +14,78 @@ package stat.ui.stats.main;
  * ******************************* *
  */
 
+import stat.domain.Sale;
+import stat.service.SaleService;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+/**
+ * Fiscal year definitions are based on those of
+ * <a href="http://en.wikipedia.org/wiki/Fiscal_year#United_States">USA</a>.
+ */
 @Component
 // Required to not run this class in a test environment
 @ConditionalOnProperty(value = "java.awt.headless", havingValue = "false")
 public class StatController {
+    @Autowired
+    private SaleService saleService;
+
+    // FY 2015: 1 October 2014 - 30 September 2015
+    private BigDecimal summarizeFiscalYear(int year) {
+        return summarizeInterval(year - 1, 10, 1, year, 9, 30);
+    }
+
+    // 1st quarter for FY 2015: 1 October 2014 – 31 December 2014
+    private BigDecimal summarizeFirstFiscalQuarter(int year) {
+        return summarizeInterval(year - 1, 10, 1, year - 1, 12, 31);
+    }
+
+    // 2nd quarter for FY 2015: 1 January 2015 – 31 March 2015
+    private BigDecimal summarizeSecondFiscalQuarter(int year) {
+        return summarizeInterval(year, 1, 1, year, 3, 31);
+    }
+
+    // 3rd quarter for FY 2015: 1 April 2015 – 30 June 2015
+    private BigDecimal summarizeThirdFiscalQuarter(int year) {
+        return summarizeInterval(year, 4, 1, year, 6, 30);
+    }
+
+    // 4th quarter for FY 2015: 1 July 2015 – 30 September 2015
+    private BigDecimal summarizeFourthFiscalQuarter(int year) {
+        return summarizeInterval(year, 7, 1, year, 9, 30);
+    }
+
+    private BigDecimal summarizeMonth(int year, int month) {
+        return summarizeInterval(year, month, 1, year, month, getNumDaysInMonth(month));
+    }
+
+    // Months are returned as 0-11 from Date class.
+    private int getNumDaysInMonth(int month) {
+        // Before (and including) July (month = 6 at Date class), odd months are 31 days.
+        // After (and including) August (month = 7 at Date class), even months are 31 days.
+        int oddOrEvenSideOfYear = (month <= 6) ? 1 : 0;
+        if (month % 2 == oddOrEvenSideOfYear) {
+            return 31;
+        } else {
+            return 30;
+        }
+    }
+
+    private BigDecimal summarizeInterval(int beginYear, int beginMonth, int beginDay, int endYear, int endMonth, int endDay) {
+        final Date from = new Date(beginYear, beginMonth, beginDay);
+        final Date until = new Date(endYear, endMonth, endDay);
+        return calculateTotalSalesForInterval(from, until);
+    }
+
+    private BigDecimal calculateTotalSalesForInterval(Date from, Date until) {
+        LinkedHashSet<Sale> sales = saleService.searchForSales(null, from, until, null, null);
+        BigDecimal totalSales = new BigDecimal(0.0);
+        sales.stream().map(sale -> totalSales.add(sale.getTotalPrice()));
+        return totalSales;
+    }
 }
