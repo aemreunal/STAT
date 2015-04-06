be76f00ddc5b3c2905754be4812fc11d73da4777
A. Emre Ünal <aemreunal@gmail.com>
2015-04-06 14:12:51 +0300

    Refactor quarter comparison

--- src/main/java/stat/ui/stats/main/StatController.java
+++ src/main/java/stat/ui/stats/main/StatController.java
@@ -16,6 +16,7 @@ package stat.ui.stats.main;
 
 import stat.domain.Sale;
 import stat.service.SaleService;
+import stat.ui.stats.main.view.StatMainPage;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
@@ -24,17 +25,16 @@ import java.util.LinkedHashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import stat.ui.stats.main.view.StatMainPage;
 
 /**
- * Fiscal year definitions are based on those of
- * <a href="http://en.wikipedia.org/wiki/Fiscal_year#United_States">USA</a>.
+ * Fiscal year definitions are based on those of <a href="http://en.wikipedia.org/wiki/Fiscal_year#United_States">USA</a>.
  */
 @Component
 // Required to not run this class in a test environment
 @ConditionalOnProperty(value = "java.awt.headless", havingValue = "false")
 public class StatController {
 
+    public static final int QUARTERS_PER_YEAR = 4;
     @Autowired
     private SaleService saleService;
 
@@ -49,10 +49,11 @@ public class StatController {
 
     public void summarizeQuarterButtonClicked(int year) {
         String title = "Summary of " + getDateFormat(year, 1, 1, "YYYY");
-        String message = "Total revenue in first quarter: " + summarizeFirstFiscalQuarter(year).toPlainString() +
-                         "\nTotal revenue in second quarter: " + summarizeSecondFiscalQuarter(year).toPlainString() +
-                         "\nTotal revenue in third quarter: " + summarizeThirdFiscalQuarter(year).toPlainString() +
-                         "\nTotal revenue in fourth quarter: " + summarizeFourthFiscalQuarter(year).toPlainString();
+        String message =
+                "Total revenue in first quarter: " + summarizeFirstFiscalQuarter(year).toPlainString() +
+                "\nTotal revenue in second quarter: " + summarizeSecondFiscalQuarter(year).toPlainString() +
+                "\nTotal revenue in third quarter: " + summarizeThirdFiscalQuarter(year).toPlainString() +
+                "\nTotal revenue in fourth quarter: " + summarizeFourthFiscalQuarter(year).toPlainString();
         statMainPage.showResult(message, title);
     }
 
@@ -64,43 +65,35 @@ public class StatController {
 
     public void compareYearButtonClicked(int firstYear, int secondYear) {
         BigDecimal firstRevenue = summarizeFiscalYear(firstYear);
-        BigDecimal secondRevenue =  summarizeFiscalYear(secondYear);
+        BigDecimal secondRevenue = summarizeFiscalYear(secondYear);
 
         String title = getDateFormat(firstYear, 1, 1, "YYYY") + " vs " + getDateFormat(secondYear, 1, 1, "YYYY");
         String message = getString("Total revenue of ", firstYear, secondYear, firstRevenue, secondRevenue);
         statMainPage.showResult(message, title);
     }
 
-    public void compareQuarterButtonClicked(int firstYear, int secondYear) {
-        BigDecimal first1QuarterRevenue = summarizeFirstFiscalQuarter(firstYear);
-        BigDecimal first2QuarterRevenue = summarizeSecondFiscalQuarter(firstYear);
-        BigDecimal first3QuarterRevenue = summarizeThirdFiscalQuarter(firstYear);
-        BigDecimal first4QuarterRevenue = summarizeFourthFiscalQuarter(firstYear);
-
-        BigDecimal second1QuarterRevenue = summarizeFirstFiscalQuarter(secondYear);
-        BigDecimal second2QuarterRevenue = summarizeSecondFiscalQuarter(secondYear);
-        BigDecimal second3QuarterRevenue = summarizeThirdFiscalQuarter(secondYear);
-        BigDecimal second4QuarterRevenue = summarizeFourthFiscalQuarter(secondYear);
-
-        String title = getDateFormat(firstYear, 1, 1, "YYYY") + " vs " + getDateFormat(secondYear, 1, 1, "YYYY");
-        String message = getString("1. Quarter of ", firstYear, secondYear, first1QuarterRevenue, second1QuarterRevenue) +
-                         "\n\n" +
-                         getString("2. Quarter of ", firstYear, secondYear, first2QuarterRevenue, second2QuarterRevenue) +
-                         "\n\n" +
-                         getString("3. Quarter of ", firstYear, secondYear, first3QuarterRevenue, second3QuarterRevenue) +
-                         "\n\n" +
-                         getString("4. Quarter of ", firstYear, secondYear, first4QuarterRevenue, second4QuarterRevenue);
-        statMainPage.showResult(message, title);
-    }
-
     public void compareMonthButtonClicked(int firstYear, int firstMonth, int secondYear, int secondMonth) {
         BigDecimal firstRevenue = summarizeMonth(firstYear, firstMonth);
-        BigDecimal secondRevenue =  summarizeMonth(secondYear, secondMonth);
+        BigDecimal secondRevenue = summarizeMonth(secondYear, secondMonth);
 
         String title = getDateFormat(firstYear, firstMonth, 1, "MMMM YYYY") + " vs " + getDateFormat(secondYear, secondMonth, 1, "MMMM YYYY");
         String message = "Total revenue of " + getDateFormat(firstYear, firstMonth, 1, "MMMM YYYY : ") + firstRevenue.toPlainString() +
-                         "\nTotal revenue of " + getDateFormat(secondYear, secondMonth, 1, "MMMM YYYY : ") + secondRevenue.toPlainString() +
-                         "\n" + getDifference(firstRevenue, secondRevenue);
+                "\nTotal revenue of " + getDateFormat(secondYear, secondMonth, 1, "MMMM YYYY : ") + secondRevenue.toPlainString() +
+                "\n" + getDifference(firstRevenue, secondRevenue);
+        statMainPage.showResult(message, title);
+    }
+
+    public void compareQuarterButtonClicked(int firstYear, int secondYear) {
+        String title = getDateFormat(firstYear, 1, 1, "YYYY") + " vs " + getDateFormat(secondYear, 1, 1, "YYYY");
+        String message = "";
+        // 4 quarters in a year
+        for (int i = 0; i < QUARTERS_PER_YEAR; i++) {
+            message += getString((i + 1) + ". quarter of ", firstYear, secondYear, summarizeFiscalQuarter(i, firstYear), summarizeFiscalQuarter(i, secondYear));
+            if (i != (QUARTERS_PER_YEAR - 1)) {
+                // Add newline except for the last quarter
+                message += "\n\n";
+            }
+        }
         statMainPage.showResult(message, title);
     }
 
@@ -110,22 +103,41 @@ public class StatController {
     }
 
     private String getString(String message, int firstYear, int secondYear, BigDecimal firstRevenue, BigDecimal secondRevenue) {
-        return message+ getDateFormat(firstYear, 1, 1, "YYYY : ") + firstRevenue.toPlainString() +
+        return message + getDateFormat(firstYear, 1, 1, "YYYY : ") + firstRevenue.toPlainString() +
                 "\n" + message + getDateFormat(secondYear, 1, 1, "YYYY : ") + secondRevenue.toPlainString() +
                 "\n" + getDifference(firstRevenue, secondRevenue);
     }
 
     private String getDifference(BigDecimal firstRevenue, BigDecimal secondRevenue) {
         switch (firstRevenue.compareTo(secondRevenue)) {
-            case -1: return "Revenue increased " + secondRevenue.subtract(firstRevenue).toPlainString();
-            case 1 : return "Revenue decreased " + firstRevenue.subtract(secondRevenue).toPlainString();
-            default: return "Revenue stayed the same";
+            case -1:
+                return "Revenue increased " + secondRevenue.subtract(firstRevenue).toPlainString();
+            case 1:
+                return "Revenue decreased " + firstRevenue.subtract(secondRevenue).toPlainString();
+            default:
+                return "Revenue stayed the same";
         }
     }
 
     // FY 2015: 1 October 2014 - 30 September 2015
     private BigDecimal summarizeFiscalYear(int year) {
         return summarizeInterval(year - 1, 10, 1, year, 9, 30);
+    }
+
+    private BigDecimal summarizeFiscalQuarter(int quarter, int year) {
+        // 4 quarters in a year
+        switch (quarter % QUARTERS_PER_YEAR) {
+            case 0:
+                return summarizeFirstFiscalQuarter(year);
+            case 1:
+                return summarizeSecondFiscalQuarter(year);
+            case 2:
+                return summarizeThirdFiscalQuarter(year);
+            case 3:
+                return summarizeFourthFiscalQuarter(year);
+            default:
+                return new BigDecimal(0.0);
+        }
     }
 
     // 1st quarter for FY 2015: 1 October 2014 – 31 December 2014
