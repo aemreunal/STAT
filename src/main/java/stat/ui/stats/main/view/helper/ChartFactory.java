6daa8dc997cf1bd13038b26b0cceb99ab539278f
Eray Tuncer <eray.tuncer@ozu.edu.tr>
2015-04-11 21:18:42 +0300

    Add other charts to ChartFactory

--- src/main/java/stat/ui/stats/main/view/helper/ChartFactory.java
+++ src/main/java/stat/ui/stats/main/view/helper/ChartFactory.java
@@ -26,7 +26,9 @@ public class ChartFactory {
                                             "July"   , "August"  , "September",
                                             "October", "November", "December"};
 
-    public static JFreeChart createMonthlyChart(Map<String, int[]> salesMap) {
+    private static final String[] QUARTERS = {"Q1", "Q2", "Q3", "Q4"};
+
+    public static JFreeChart createMonthChart(Map<String, int[]> salesMap) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
         for (String yearName : salesMap.keySet()) {
             int[] sales = salesMap.get(yearName);
@@ -35,6 +37,25 @@ public class ChartFactory {
             }
         }
         return org.jfree.chart.ChartFactory.createBarChart("Monthly Sales", "Months", "#Sales", dataset);
+    }
+
+    public static JFreeChart createQuarterChart(Map<String, int[]> salesMap) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+        for (String yearName : salesMap.keySet()) {
+            int[] sales = salesMap.get(yearName);
+            for (int quarterIndex = 0; quarterIndex < QUARTERS.length; quarterIndex++) {
+                dataset.addValue(sales[quarterIndex], yearName, QUARTERS[quarterIndex]);
+            }
+        }
+        return org.jfree.chart.ChartFactory.createBarChart("Quarterly Sales", "Quarters", "#Sales", dataset);
+    }
+
+    public static JFreeChart createYearChart(Map<String, Integer> salesMap) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+        for (String yearName : salesMap.keySet()) {
+            dataset.addValue(salesMap.get(yearName), yearName, yearName);
+        }
+        return org.jfree.chart.ChartFactory.createBarChart("Yearly Sales", "Years", "#Sales", dataset);
     }
 
 }
